873dae0bb75f12bcee53dd9650fea207ccc3694b
xinyi234 <lijialin8917@qq.com>
2024-10-07 18:46:04 +0800

    first commit

---
+++ surprise.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button')
+btn.addEventListener('click',()=>{
+    document.body.querySelector('div.surprise').style.display = 'block';
+    document.body.style.background = 'url(img/jide.jpeg)',no-repeat,center,center;
+    
+})
